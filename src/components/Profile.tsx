0bcd1a33e4c6b2354209ad84688414de52f1eb47
brunoBMonteiro <brunobilheri@hotmail.com>
2021-02-23 18:15:33 -0300

    criando a home

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+    return (
+        <div>
+            <img src="https://github.com/brunoBMonteiro.png" alt="Bruno B Monteiro" />
+            <div>
+                <strong>Bruno B Monteiro</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from '../components/Profile';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+        </div>
+        <div>
+
+        </div>
+      </section>
+    </div>
+  )
+}
