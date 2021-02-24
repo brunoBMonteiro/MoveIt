7baa483c170350d4dd5f26cf9a727640feafa6c4
brunoBMonteiro <brunobilheri@hotmail.com>
2021-02-24 17:36:00 -0300

    componente Countdown criado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
 
@@ -8,12 +9,17 @@ import styles from '../styles/pages/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | Move It</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
         <div>
 
