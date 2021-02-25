5895ad0e7b036adfe84ffa18fbe41f9b26337fde
brunoBMonteiro <brunobilheri@hotmail.com>
2021-02-25 16:55:00 -0300

    Iniciando as regras de negocio

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,11 +1,17 @@
 import { createContext, ReactNode, useState } from 'react';
+import challenges from '../../challenges.json';
 
-
+interface Challenge {
+    type: 'body' | 'eye';
+    description: string;
+    amount: number;
+}
 
 interface ChallengesContextData {
     level: number;
     currentExperience: number;
     challengesCompleted: number;
+    activeChallenge: Challenge;
     levelUp: () => void;
     startNewChallenge: () => void;
 }
@@ -21,12 +27,17 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     const [currentExperience, setCurrentExperience] = useState(0);
     const [challengesCompleted, setChallengesCompleted] = useState(0);
 
+    const [activeChallenge, setActiveChallenge] = useState(null)
+
     function levelUp() {
         setLevel(level + 1);
     }
 
     function startNewChallenge() {
-        console.log('New challenge');
+        const randomChallengeIndex = Math.floor(Math.random() * challenges.length)
+        const challenge = challenges[randomChallengeIndex];
+
+        setActiveChallenge(challenge)
     }
 
     return (
@@ -37,6 +48,7 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
                 challengesCompleted,
                 levelUp,
                 startNewChallenge,
+                activeChallenge,
             }}
         >
             {children}
